57be1f4b4219fcb4db9de6036dbd2bbad6c07154
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-04-21 02:23:14 +0600

    first commit

---
+++ search.js
@@ -0,0 +1,14 @@
+const products = [
+    {name: "i Phone", price: 70000},
+    {name: "samsung", price: 20000},
+    {name: "huawei", price: 27000},
+    {name: "pixel phone", price: 65000},
+    {name: "rog laptop", price: 230000},
+    {name: "symphony", price: 6500},
+    {name: "samsung watch", price: 6500},
+    {name: "asus laptop", price: 65000},
+    {name: "apple watch", price: 20000},
+]
+function searchProducts(search, searchText){
+    
+}
